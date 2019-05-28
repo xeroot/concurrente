e3b061f29474c12e8cf03dab28fde6f28a02cc67
xeroot <r10500@hotmail.com>
2019-05-28 12:46:29 -0500

    servidor y cliente en go
    nope

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"fmt"
+	"net"
+	"bufio"
+)
+
+func handle(con net.Conn) {
+	defer con.Close()
+	r := bufio.NewReader(con)
+	for {
+		// aqui se chanta esperando otro request (otra linea)
+		msg, _ := r.ReadString('\n')
+		fmt.Fprintln(con, msg) // aqui le responde al cliente
+		fmt.Println("Recibido: ", msg)
+		if len(msg) > 0 && msg[0] == 'x' {
+			break;
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Iniciando Servidor...")
+	ln, _:= net.Listen("tcp", "localhost:8000")
+	defer ln.Close()
+	fmt.Println("Escuchando por puerto 8000")
+	for {
+		// aqui se quede esperando a un cli
+		// OJO, no cada request sino un cliente
+		con, _ := ln.Accept()
+		go handle(con)
+	}
+}
