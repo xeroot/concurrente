e40fb1e6319aaabea5aefd3fc9f5ac58d2c2fba8
xeroot <r10500@hotmail.com>
2019-06-12 14:59:27 -0500

    producto punto cambiado y producto cruz resuelto
    Aun queda agregar los inputs del usuario...

--- TA2.cpp
+++ TA2.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 #include <omp.h>
 #define N 3
+#define N1 2
+#define N2 2
+#define N3 3
 using namespace std;
 // parametros de lanzamiento '-std=c++14 -fopenmp'
 void imprimir(int C[N][N]) {
@@ -53,22 +56,38 @@ void producto_punto(int A[N][N], int B[N][N]) {
 		int id = omp_get_thread_num();
 		int i = id%N;
 		int j = id/N;
-		for(int p=0; p<N; p++) C[i][j]+=A[i][p]*B[p][j];
+		C[i][j] = A[i][j]*B[i][j];
 	}
 	cout << "Resultado Producto Punto:" << endl;
 	imprimir(C);
 }
-void producto_cruz(int A[N][N], int B[N][N]) {
-	int C[N][N] = {{0,0,0},{0,0,0},{0,0,0}};
-	#pragma omp parallel num_threads(N*N)
+void producto_cruz(int arr[]) {
+	int A[N1][N2] = {
+		{2,1},
+		{1,0}
+	};
+	int B[N2][N3] = {
+		{1,2,3},
+		{3,2,1}
+	};
+	int C[N1][N3]= {
+		{0,0,0},
+		{0,0,0}
+	};
+	#pragma omp parallel num_threads(N1*N3)
 	{
 		int id = omp_get_thread_num();
-		int i = id%N;
-		int j = id/N;
-		// ...
+		int i = id%N2;
+		int j = id/N2;
+		for(int p=0; p<N2; p++) C[i][j]+=A[i][p]*B[p][j];
 	}
+	
 	cout << "Resultado Producto Cruz:" << endl;
-	imprimir(C);
+	for(int i=0; i<N1; i++) {
+		for(int j=0; j<N3; j++) cout << C[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
 }
 
 int main() {
@@ -87,6 +106,7 @@ int main() {
 	resta(A,B);
 	multi_escalar(A,E);
 	producto_punto(A,B);
-	producto_cruz(A,B);
+	producto_cruz();	
+	
 	return 0;
 }
