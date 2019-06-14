d31afd0e5066cb846b68d9e0e07f59c8434ff36b
xeroot <r10500@hotmail.com>
2019-06-14 15:55:43 -0500

    bug fixed e ingreso de datos agregado

--- TA2.cpp
+++ TA2.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 #include <omp.h>
-#define N 3
-#define N1 2
-#define N2 2
-#define N3 3
 using namespace std;
 // parametros de lanzamiento '-std=c++14 -fopenmp'
-void imprimir(int C[N][N]) {
+
+void imprimir(int** C, int N, int M) {
 	for(int i=0; i<N; i++) {
-		for(int j=0; j<N; j++) cout << C[i][j] << " ";
+		for(int j=0; j<M; j++) cout << C[i][j] << " ";
 		cout << endl;
 	}
 	cout << endl;
 }
-void suma(int A[N][N], int B[N][N]) {
-	int C[N][N] = {{0,0,0},{0,0,0},{0,0,0}};
+
+int** create_matrix(int N,int M){
+	int **matriz = new int*[N];
+	for(int i=0;i<M;i++) matriz[i]= new int[N];
+	for(int i=0;i<N*M;i++) cin >> matriz[i/M][i%M];
+	imprimir(matriz, N, M);
+	return matriz;
+}
+
+int** get_empty_matrix(int N,int M){
+	int **matriz = new int*[N];
+	for(int i=0;i<M;i++) matriz[i]= new int[N];
+	for(int i=0;i<N*M;i++) matriz[i/M][i%M] = 0;
+	return matriz;
+}
+
+void suma() {
+	int N;
+	cout << "N: ";
+	cin >> N;
+	cout << "Matriz A ("<<N*N<<"): ";
+	int **A = create_matrix(N,N);
+	cout << "Matriz B ("<<N*N<<"): ";
+	int **B = create_matrix(N,N);
+	int **C = get_empty_matrix(N,N);
 	#pragma omp parallel num_threads(N*N)
 	{
 		int id = omp_get_thread_num();
@@ -23,10 +43,18 @@ void suma(int A[N][N], int B[N][N]) {
 		C[i][j] = A[i][j]+B[i][j];
 	}
 	cout << "Resultado Suma:" << endl;
-	imprimir(C);
+	imprimir(C, N, N);
 }
-void resta(int A[N][N], int B[N][N]) {
-	int C[N][N] = {{0,0,0},{0,0,0},{0,0,0}};
+
+void resta() {
+	int N;
+	cout << "N: ";
+	cin >> N;
+	cout << "Matriz A ("<<N*N<<"): ";
+	int **A = create_matrix(N,N);
+	cout << "Matriz B ("<<N*N<<"): ";
+	int **B = create_matrix(N,N);
+	int **C = get_empty_matrix(N,N);
 	#pragma omp parallel num_threads(N*N)
 	{
 		int id = omp_get_thread_num();
@@ -35,10 +63,17 @@ void resta(int A[N][N], int B[N][N]) {
 		C[i][j] = A[i][j]-B[i][j];
 	}
 	cout << "Resultado Resta:" << endl;
-	imprimir(C);
+	imprimir(C, N, N);
 }
-void multi_escalar(int A[N][N], double E) {
-	int C[N][N] = {{0,0,0},{0,0,0},{0,0,0}};
+void multi_escalar() {
+	int N;
+	cout << "N: ";
+	cin >> N;
+	cout << "Matriz A ("<<N*N<<"): ";
+	int **A = create_matrix(N,N);
+	cout << "Punto escalar: ";
+	int E; cin >> E;
+	int **C = get_empty_matrix(N,N);
 	#pragma omp parallel num_threads(N*N)
 	{
 		int id = omp_get_thread_num();
@@ -46,11 +81,18 @@ void multi_escalar(int A[N][N], double E) {
 		int j = id/N;
 		C[i][j] = A[i][j]*E;
 	}
-	cout << "Resultado Multi. Escalar:" << endl;
-	imprimir(C);
+	cout << "Resultado Mult. Escalar:" << endl;
+	imprimir(C, N, N);
 }
-void producto_punto(int A[N][N], int B[N][N]) {
-	int C[N][N] = {{0,0,0},{0,0,0},{0,0,0}};
+void producto_punto() {
+	int N;
+	cout << "N: ";
+	cin >> N;
+	cout << "Matriz A ("<<N*N<<"): ";
+	int **A = create_matrix(N,N);
+	cout << "Matriz B ("<<N*N<<"): ";
+	int **B = create_matrix(N,N);
+	int **C = get_empty_matrix(N,N);
 	#pragma omp parallel num_threads(N*N)
 	{
 		int id = omp_get_thread_num();
@@ -59,21 +101,18 @@ void producto_punto(int A[N][N], int B[N][N]) {
 		C[i][j] = A[i][j]*B[i][j];
 	}
 	cout << "Resultado Producto Punto:" << endl;
-	imprimir(C);
+	imprimir(C, N, N);
 }
-void producto_cruz(int arr[]) {
-	int A[N1][N2] = {
-		{2,1},
-		{1,0}
-	};
-	int B[N2][N3] = {
-		{1,2,3},
-		{3,2,1}
-	};
-	int C[N1][N3]= {
-		{0,0,0},
-		{0,0,0}
-	};
+
+void producto_cruz() {
+	int N1, N2, N3;
+	cout << "N1, N2 y N3: ";
+	cin >> N1 >> N2 >> N3;
+	cout << "Matriz A ("<<N1*N2<<"): ";
+	int **A = create_matrix(N1,N2);
+	cout << "Matriz B ("<<N2*N3<<"): ";
+	int **B = create_matrix(N2,N3);
+	int **C = get_empty_matrix(N1,N3);
 	#pragma omp parallel num_threads(N1*N3)
 	{
 		int id = omp_get_thread_num();
@@ -83,30 +122,41 @@ void producto_cruz(int arr[]) {
 	}
 	
 	cout << "Resultado Producto Cruz:" << endl;
-	for(int i=0; i<N1; i++) {
-		for(int j=0; j<N3; j++) cout << C[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
+	imprimir(C, N1, N3);
+}
+
+void menu(){
+	cout << "Opciones:" << endl;
+	cout << "1. Suma" << endl;
+	cout << "2. Resta" << endl;
+	cout << "3. Mult. por Escalar" << endl;
+	cout << "4. Prod. Punto" << endl;
+	cout << "5. Prod. Cruz" << endl;
 }
 
 int main() {
-	int A[N][N] = {
-		{1,2,3},
-		{4,5,6},
-		{7,8,9}
-	};
-	int B[N][N] = {
-		{1,0,2},
-		{0,1,2},
-		{1,0,0}
-	};
-	double E = 4;
-	suma(A,B);
-	resta(A,B);
-	multi_escalar(A,E);
-	producto_punto(A,B);
-	producto_cruz();	
-	
+	menu();
+	int opcion=0;
+	do{
+		cout << "Escoja opcion: ";
+		cin >> opcion;
+	}while(opcion < 1 || opcion > 5);
+	switch(opcion){
+		case 1:
+			suma();
+		break;
+		case 2:
+			resta();
+		break;
+		case 3:
+			multi_escalar();
+		break;
+		case 4:
+			producto_punto();
+		break;
+		case 5:
+			producto_cruz();
+		break;
+	}
 	return 0;
 }
