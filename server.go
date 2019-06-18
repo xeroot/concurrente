1f70bafe0f06d3156120816963210fc175ab5fe0
xeroot <r10500@hotmail.com>
2019-06-18 19:56:10 +0000

    theia test

--- server.go
+++ server.go
@@ -1,27 +1,29 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"bufio"
 )
 
 func handle(con net.Conn) {
 	defer con.Close()
 	r := bufio.NewReader(con)
 	for {
+		// test theia xd
 		// aqui se chanta esperando otro request (otra linea)
 		msg, _ := r.ReadString('\n')
 		fmt.Fprintln(con, msg) // aqui le responde al cliente
 		fmt.Println("Recibido: ", msg)
 		if len(msg) > 0 && msg[0] == 'x' {
-			break;
+			break
 		}
 	}
 }
 
 func main() {
 	fmt.Println("Iniciando Servidor...")
-	ln, _:= net.Listen("tcp", "localhost:8000")
+	ln, _ := net.Listen("tcp", "localhost:8000")
 	defer ln.Close()
 	fmt.Println("Escuchando por puerto 8000")
 	for {
